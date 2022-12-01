ca1ac569bef13d4d8b11c6c6f3ec6db98d634d98
leonardog9823 <guanuco43@gmail.com>
2022-11-30 21:08:51 -0300

    fix css modules production

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: isProd ? 'https://cdn.mydomain.com' : undefined,
 }
 
 module.exports = nextConfig
