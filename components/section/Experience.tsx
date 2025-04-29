23c9e20726f97d40c93c7a9cf7c3ed17a560c8d7
Leonardo-G <guanuco43@gmail.com>
2025-04-28 23:54:09 -0300

    Add experience badge and install embla carousel

--- components/section/Experience.tsx
+++ components/section/Experience.tsx
@@ -5,7 +5,17 @@ export default function Experience() {
     <section className='mx-auto my-30 w-full max-w-[1280px]'>
       <Title title='Experiencia' />
       <div>
-        <div className='w-full shadow-[0px_4px_4px_rgba(0,0,0,0.25)]'></div>
+        <div className='text-grey-primary font-open-sans flex w-full justify-between px-10 py-4 tracking-[0.3px] shadow-[0px_4px_4px_rgba(0,0,0,0.25)]'>
+          <div>
+            <p className='text-xl'>Software Developer</p>
+            <p className='mt-2 text-sm'>Septiembre 2023 - Presente</p>
+          </div>
+          <div>
+            <p className='text-xl'>Bigger Tech</p>
+            <p className='mt-2 text-sm'>Remoto</p>
+          </div>
+          <div className='w-[30%]'></div>
+        </div>
       </div>
     </section>
   );
