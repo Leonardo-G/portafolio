1e7d2e0d411309d78f0b8dfaf97c421ae8fce92b
Leonardo-G <guanuco43@gmail.com>
2025-04-30 18:37:37 -0300

    Add controls en carousel

--- components/section/Embla.tsx
+++ components/section/Embla.tsx
@@ -1,7 +1,8 @@
 'use client';
-import React from 'react';
+import React, { useCallback } from 'react';
 import { EmblaOptionsType } from 'embla-carousel';
 import useEmblaCarousel from 'embla-carousel-react';
+import { IoIosArrowForward, IoIosArrowBack } from 'react-icons/io';
 
 type PropType = {
   slides: string[];
@@ -10,20 +11,39 @@ type PropType = {
 
 const EmblaCarousel: React.FC<PropType> = (props) => {
   const { slides } = props;
-  const [emblaRef] = useEmblaCarousel();
+  const [emblaRef, emblaApi] = useEmblaCarousel();
+
+  const handleScrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
+
+  const handleScrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
 
   return (
-    <section className='embla'>
-      <div className='overflow-hidden' ref={emblaRef}>
-        <div className='font-open-sans flex gap-x-3'>
-          {slides.map((value) => (
-            <div className='bg-grey-badge rounded-[30px] px-4 py-1' key={value}>
-              <div className='text-xs select-none'>{value}</div>
-            </div>
-          ))}
-        </div>
+    <div className='flex items-center gap-x-2'>
+      <div className='cursor-pointer' onClick={handleScrollPrev}>
+        <IoIosArrowBack />
       </div>
-    </section>
+      <section className='embla overflow-hidden'>
+        <div className='overflow-hidden' ref={emblaRef}>
+          <div className='font-open-sans flex gap-x-3'>
+            {slides.map((value) => (
+              <div
+                className='bg-grey-badge rounded-[30px] px-4 py-1'
+                key={value}
+              >
+                <div className='text-xs select-none'>{value}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+      <div className='cursor-pointer' onClick={handleScrollNext}>
+        <IoIosArrowForward />
+      </div>
+    </div>
   );
 };
 
